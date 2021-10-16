ecf54d0efaea17f9732397ec09369202a881cb77
jalb850420 <70179616+jalb850420@users.noreply.github.com>
2021-10-16 18:06:56 -0500

    Actualizacion link formularios
    entre Home y la pagina Inicio

--- proyectflask/proyect/App.py
+++ proyectflask/proyect/App.py
@@ -26,7 +26,7 @@ listaprov = {
 def Home():
     return render_template('home.html')
 
-@App.route('/Inicio', methods=["GET"])
+@App.route('/Inicio', methods=["GET","POST"])
 def Inicio():
     return render_template('inicio.html')
 
