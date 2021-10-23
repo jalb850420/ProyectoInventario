c823a486cfacb0bdfc62955ab36cc130620a7169
jalb850420 <70179616+jalb850420@users.noreply.github.com>
2021-10-23 13:01:59 -0500

    Instalacion de comandos
    pip install gunicorn
    pip install python-decouple
    pip freeze > requirements.txt

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import os
 from flask import Flask, request, render_template
 
 
-
-
-from database import sql_insert_tipousuario, sql_select_productos, sql_edit_producto, sql_delete_producto
+from database import sql_insert_tipousuario, sql_select_productos#, sql_edit_producto, sql_delete_producto
 from forms import  TipoUsuario,Producto
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -33,12 +31,12 @@ def editar_producto():
     codigo = request.args.get('codigo') #captura de la vble código enviada a través de la URL
     nombre = request.args.get('nombre') #captura de la vble nombre enviada a través de la URL
     cantidad = request.args.get('cantidad') #captura de la vble cantidad enviada a través de la URL
-    sql_edit_producto(id, codigo, nombre, cantidad) #llamado de la función de edición de la base de datos
+ #   sql_edit_producto(id, codigo, nombre, cantidad) #llamado de la función de edición de la base de datos
     return "OK"
 
 @app.route('/delete', methods=['GET'])
 def borrar_producto():
 	id = request.args.get('id') #captura de la variable id enviada a través de la URL
-	sql_delete_producto(id) #llamado a la función de borrado de la base de datos
+#sql_delete_producto(id) #llamado a la función de borrado de la base de datos
 	return "OK"
 
